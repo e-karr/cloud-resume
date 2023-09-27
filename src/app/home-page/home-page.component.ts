54200f0ec2c034614bffff86d957331c839083bd
Elizabeth Karr <e.karr93@icloud.com>
2023-09-26 19:12:03 -0500

    code and style home-page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
-
+  menuItems: string[] = [
+    'About',
+    'Experience',
+    'Skills',
+    'Projects',
+    'Education',
+    'Volunteer',
+  ];
 }
