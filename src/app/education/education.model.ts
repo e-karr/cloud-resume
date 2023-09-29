2b39b8c43c72208ed194882e0583efb428049892
Elizabeth Karr <e.karr93@icloud.com>
2023-09-28 19:04:46 -0500

    edit location field

--- src/app/education/education.model.ts
+++ src/app/education/education.model.ts
@@ -1,19 +1,25 @@
 export class Education {
-    public school: string;
-    public city: string;
-    public state: string;
-    public degree: string;
-    public major: string;
-    public imagePath: string;
-    public certificatePath: string | undefined;
+  public school: string;
+  public location: string;
+  public state: string | undefined;
+  public degree: string;
+  public major: string;
+  public imagePath: string;
+  public certificatePath: string | undefined;
 
-    constructor (school: string, degree: string, major: string, imagePath: string, city: string, state: string, certificatePath?: string | undefined) {
-        this.school = school;
-        this.city = city;
-        this.state = state;
-        this.degree = degree;
-        this.major = major;
-        this.imagePath = imagePath;
-        this.certificatePath = certificatePath;
-    }
+  constructor(
+    school: string,
+    degree: string,
+    major: string,
+    imagePath: string,
+    location: string,
+    certificatePath?: string | undefined
+  ) {
+    this.school = school;
+    this.location = location;
+    this.degree = degree;
+    this.major = major;
+    this.imagePath = imagePath;
+    this.certificatePath = certificatePath;
+  }
 }
