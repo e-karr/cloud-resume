4b7d29a4249b096cc25ced1a5a338e4dd41e280f
Elizabeth Karr <e.karr93@icloud.com>
2023-09-30 18:37:03 -0500

    complete skills component

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkillsService } from './skills.service';
+import { Skill } from './skill.model';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
+  languages: Skill[] = [];
+  frameworks: Skill[] = [];
+  tools: Skill[] = [];
 
+  constructor(private skillsService: SkillsService) {}
+
+  ngOnInit(): void {
+    this.languages = this.skillsService.getLanguageList();
+    this.frameworks = this.skillsService.getFrameworkList();
+    this.tools = this.skillsService.getToolList();
+  }
 }
--- src/app/skills/skills.service.ts
+++ src/app/skills/skills.service.ts
@@ -2,35 +2,35 @@ import { Skill } from './skill.model';
 
 export class SkillsService {
   private languageList: Skill[] = [
-    new Skill('JavaScript', 'language', 'assets/images/skills/javascript.png'),
-    new Skill('TypeScript', 'language', 'assets/images/skills/typescript.png'),
-    new Skill('Java', 'language', 'assets/images/skills/java.png'),
-    new Skill('MySQL', 'language', 'assets/images/skills/sql.png'),
-    new Skill('HTML', 'language', 'assets/images/skills/html.png'),
-    new Skill('CSS', 'language', 'assets/images/skills/css.png'),
-    new Skill('Python', 'language', 'assets/images/skills/python.png'),
+    new Skill('JavaScript', 'language', 'assets/images/skills/javascript.svg'),
+    new Skill('TypeScript', 'language', 'assets/images/skills/typescript.svg'),
+    new Skill('Java', 'language', 'assets/images/skills/java.svg'),
+    new Skill('MySQL', 'language', 'assets/images/skills/mysql.svg'),
+    new Skill('HTML', 'language', 'assets/images/skills/html.svg'),
+    new Skill('CSS', 'language', 'assets/images/skills/css.svg'),
+    new Skill('Python', 'language', 'assets/images/skills/python.svg'),
   ];
 
   private frameworkList: Skill[] = [
-    new Skill('Angular', 'framework', 'assets/images/skills/angular.png'),
+    new Skill('Angular', 'framework', 'assets/images/skills/angular.svg'),
     new Skill(
       'Spring Boot',
       'framework',
-      'assets/images/skills/springboot.png'
+      'assets/images/skills/springboot.svg'
     ),
-    new Skill('Bootstrap', 'framework', 'assets/images/skills/bootstrap.png'),
-    new Skill('Flask', 'framework', 'assets/images/skills/flask.png'),
+    new Skill('Bootstrap', 'framework', 'assets/images/skills/bootstrap.svg'),
+    new Skill('Flask', 'framework', 'assets/images/skills/flask.svg'),
   ];
 
   private toolList: Skill[] = [
-    new Skill('AWS', 'tool', 'assets/images/skills/aws.png'),
-    new Skill('Git', 'tool', 'assets/images/skills/git.png'),
+    new Skill('Amazon Web Services', 'tool', 'assets/images/skills/aws.svg'),
+    new Skill('Git', 'tool', 'assets/images/skills/git.svg'),
     new Skill(
       'Visual Studio Code',
       'tool',
-      'assets/images/skills/visual-studio-code.png'
+      'assets/images/skills/visual-studio-code.svg'
     ),
-    new Skill('IntelliJ', 'tool', 'assets/images/skills/intellij.png'),
+    new Skill('IntelliJ', 'tool', 'assets/images/skills/intellij.svg'),
   ];
 
   getLanguageList() {
