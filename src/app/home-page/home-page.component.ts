6d3845eccd0169a96f0909533b8f65a2398954f8
Elizabeth Karr <e.karr93@icloud.com>
2023-10-01 14:53:31 -0500

    remove volunteer navigation item

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -12,6 +12,5 @@ export class HomePageComponent {
     'Skills',
     'Projects',
     'Education',
-    'Volunteer',
   ];
 }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -12,6 +12,5 @@ export class NavigationComponent {
     'Skills',
     'Projects',
     'Education',
-    'Volunteer',
   ];
 }
