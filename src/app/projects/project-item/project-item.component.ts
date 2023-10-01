c12847238f361f4d9d019a3290d1ec697debfaa0
Elizabeth Karr <e.karr93@icloud.com>
2023-10-01 10:47:47 -0500

    complete project-item component and loop in projects component

--- src/app/projects/project-item/project-item.component.ts
+++ src/app/projects/project-item/project-item.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../project.model';
+import { BulletPoint } from 'src/app/bullet-point.model';
 
 @Component({
   selector: 'app-project-item',
   templateUrl: './project-item.component.html',
-  styleUrls: ['./project-item.component.css']
+  styleUrls: ['./project-item.component.css'],
 })
 export class ProjectItemComponent {
-
+  @Input() projectItem!: Project;
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from './projects.service';
+import { Project } from './project.model';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [];
 
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit(): void {
+    this.projects = this.projectsService.getProjectList();
+  }
 }
