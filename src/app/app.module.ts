a48139366d38b1a9b502d55cf393f09db9f804c1
Elizabeth Karr <e.karr93@icloud.com>
2023-10-03 18:44:44 -0500

    generate visitor-count service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,7 @@ import { JobItemComponent } from './experience/job-item/job-item.component';
     ExperienceComponent,
     JobItemComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
     EducationService,
     SkillsService,
