1ac0abda4711b9bdf60b5f53d482e1b981665baf
Elizabeth Karr <e.karr93@icloud.com>
2023-12-03 14:07:56 -0600

    add visitor count

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  visitorCount!: number;
 
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getVisitorCount();
+  }
+
+  getVisitorCount() {
+    this.http
+      .get<number>(
+        'https://72yrg7d234xjeza7xymuprl4je0kjpun.lambda-url.us-east-1.on.aws/'
+      )
+      .subscribe((response) => {
+        this.visitorCount = response;
+      });
+  }
 }
